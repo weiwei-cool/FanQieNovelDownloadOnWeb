5a3d3fe8e6586a84b9fafc89a27815b3a03e62e9
shingyu <git@x1v.icu>
2023-11-24 01:59:12 +0800

    fix: 修改默认路径适配Windows并加入警告

--- tools/DownloadNovel.py
+++ tools/DownloadNovel.py
@@ -37,7 +37,9 @@ class DownloadNovel(threading.Thread):
         # 自定义保存路径
         self.custom_path = os.environ.get('CUSTOM_PATH')
         if not self.custom_path:
-            self.custom_path = '/root/alist/book/books'
+            self.custom_path = './books'
+            os.makedirs(self.custom_path, exist_ok=True)
+            tools.logger.warning(f'您未设置自定义保存路径，将使用默认路径: {self.custom_path}')
 
         super().__init__()
 
